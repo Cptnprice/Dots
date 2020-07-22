33afd62088002c9cc39ff13c4731231b4d921209
shalva <shalvachkuaselidze77@gmail.com>
2020-07-22 14:11:42 +0400

    connect start and end nodes

--- js/creategraph.js
+++ js/creategraph.js
@@ -1,5 +1,6 @@
 var coords = [];
 const l = [];
+let i = 1;
 
 function MouseClickCoords(e){
   var mouseX = parseInt(e.clientX);
@@ -29,57 +30,68 @@ function createelement(){
 
 document.body.addEventListener('click',createelement,false);
 
+function connectTwoNodes(i, i2){
+  let b = document.getElementById(i.toString())
+  b.style.background = "black";
+  x1 = coords[i][0];
+  y1 = coords[i][1];
+  x2 = coords[i2][0];
+  y2 = coords[i2][1];
+  let distance = Math.sqrt(((x2 - x1)**2 + (y2 - y1)**2));
+  let i1 = Math.abs(y2 - y1);
+  let a = Math.asin(i1 / distance) * (180 / Math.PI);
+  let div1 = document.createElement("div");
+  div1.style.position = "absolute";
+  div1.style.height = "1px";
+  div1.style.width = "200px";
+  let t = 0;
+  div1.style.left = coords[i][0].toString() + "px";
+  div1.style.top = coords[i][1].toString() + "px";
+  div1.style.background = "black";
+  document.body.appendChild(div1);
+  if (x1 <= x2 && y1 > y2){
+    div1.style.transform = `rotate(-${a}deg)`;
+  }
+  else if(x1 <= x2 && y1 <= y2){
+    div1.style.transform = `rotate(${a}deg)`;
+  }
+  else if(x1 > x2 && y1 >= y2){
+    div1.style.transform = `rotate(-${180 - a}deg)`;
+  }
+  else if(x1 > x2 && y1 < y2){
+    div1.style.transform = `rotate(${180 - a}deg)`;
+  }
+  div1.style.transformOrigin = "0% 0% 0";
+  div1.style.zIndex = "-1";
+  let id = setInterval(func,10);
+  function func(){
+    if (t >= distance){
+      clearInterval(id);
+    }
+    else{
+      t = t + 10;
+      div1.style.width = t.toString() + "px";
+    }
+  }
+  l.push(i);
+  l.push(i+1);
+  console.log(i,i+1);
+}
+
 function connect(){
   for (let i = 0; i < coords.length - 1; i++){
       if (l.includes(i) && l.includes(i+1)){
         continue;
       }
       else{
-        let b = document.getElementById(i.toString())
-        b.style.background = "black";
-        x1 = coords[i][0];
-        y1 = coords[i][1];
-        x2 = coords[i+1][0];
-        y2 = coords[i+1][1];
-        let distance = Math.sqrt(((x2 - x1)**2 + (y2 - y1)**2));
-        let i1 = Math.abs(y2 - y1);
-        let a = Math.asin(i1 / distance) * (180 / Math.PI);
-        let div1 = document.createElement("div");
-        div1.style.position = "absolute";
-        div1.style.height = "1px";
-        div1.style.width = "200px";
-        let t = 0;
-        div1.style.left = coords[i][0].toString() + "px";
-        div1.style.top = coords[i][1].toString() + "px";
-        div1.style.background = "black";
-        document.body.appendChild(div1);
-        if (x1 <= x2 && y1 > y2){
-          div1.style.transform = `rotate(-${a}deg)`;
-        }
-        else if(x1 <= x2 && y1 <= y2){
-          div1.style.transform = `rotate(${a}deg)`;
-        }
-        else if(x1 > x2 && y1 >= y2){
-          div1.style.transform = `rotate(-${180 - a}deg)`;
-        }
-        else if(x1 > x2 && y1 < y2){
-          div1.style.transform = `rotate(${180 - a}deg)`;
-        }
-        div1.style.transformOrigin = "0% 0% 0";
-        div1.style.zIndex = "-1";
-        let id = setInterval(func,10);
-        function func(){
-          if (t >= distance){
-            clearInterval(id);
-          }
-          else{
-            t = t + 10;
-            div1.style.width = t.toString() + "px";
-          }
-        }
-        l.push(i);
-        l.push(i+1);
-        console.log(i,i+1);
+        connectTwoNodes(i, i+1);
       }
+  }
+}
+
+function connectStartAndEnd(){
+  if (i){
+    connectTwoNodes(coords.length-1, 0);
+    i = 0;
   }
 }
